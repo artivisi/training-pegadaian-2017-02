a85c75f3d9628fc9e191abbbbc1a10855d187bcc
Endy Muhardin <endy.muhardin@gmail.com>
2017-02-20 15:26:18 +0700

    implement routing sederhana

--- gadai-frontend/src/app/app.module.ts
+++ gadai-frontend/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes }   from '@angular/router';
 
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/login/login.component';
+import { RegistrasiComponent } from './shared/registrasi/registrasi.component';
 
-
+const routingAplikasi : Routes = [
+	{path : 'login', component : LoginComponent},
+	{path : 'registrasi', component : RegistrasiComponent}
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +23,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
+	RouterModule.forRoot(routingAplikasi),
 	SharedModule
   ],
   providers: [],
--- gadai-frontend/src/app/shared/shared.module.ts
+++ gadai-frontend/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule }   from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrasiComponent } from './registrasi/registrasi.component';
@@ -7,9 +8,10 @@ import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+	RouterModule
   ],
   declarations: [ NavbarComponent, LoginComponent, RegistrasiComponent, FooterComponent ],
-  exports: [ NavbarComponent, FooterComponent ]
+  exports: [ NavbarComponent, FooterComponent, LoginComponent, RegistrasiComponent ]
 })
 export class SharedModule { }
